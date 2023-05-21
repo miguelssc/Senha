fb18c1a3b757335e662cb269f66fcf75be68bc02
miguel <miguelpositivo02@gmail.com>
2023-05-21 18:05:02 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string senha;
+Console.WriteLine("digite sua senha");
+senha = Console.ReadLine()!;
+if (senha == "1234abcd" ){
+Console.WriteLine("Acesso permitido");
+}
+else{
+Console.WriteLine("Acesso negado");
+}
